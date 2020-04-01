d56b245ebe5af292e712131257c0c7c259859ea3
mahon <mahonko_yana@mail.ru>
2020-04-01 16:47:42 +0300

    Update

--- src/main/java/PrefixTrie/PrefixTrie.java
+++ src/main/java/PrefixTrie/PrefixTrie.java
@@ -8,22 +8,16 @@ public class PrefixTrie {
     public static class Node {
         char inputName; //создаём узел с разветвлениями
         ArrayList<Node> next = new ArrayList<Node>();
-
-        public Node(char c, ArrayList<Node> list) {
+        boolean leaf; //флаг окончания
+        public Node(char c, ArrayList<Node> list, boolean flag) {
             inputName = c;
             next = list;
+            leaf = flag;
         }
-        public Node () {
-        }
-
-
         @Override
         public int hashCode() {
             return Objects.hash(inputName, next);
         }
-
-
-
         @Override
         public boolean equals(Object o) {
             if (o instanceof Node) {
@@ -32,28 +26,10 @@ public class PrefixTrie {
         }
     }
 
-
-    Node root = new Node('.', new ArrayList<PrefixTrie.Node>()); //корень с разветвлениями
-    private Exception ThrowEx (String s) {
-        if (s.length() == 0 || s.equals(".") || s.matches("[0-9]+")) {
-            throw new IllegalArgumentException();
-        }
-        return null;
+    Node root = new Node('.', new ArrayList<PrefixTrie.Node>(), false); //корень с разветвлениями
+    private boolean ThrowEx (String s) {
+        return s.length() == 0 || s.equals(".") || s.matches("[0-9]+");
     }
-
-
-    private static ArrayList<Character> endIsEmpty(List<Node> nodes){
-        ArrayList<Character> need = new ArrayList<Character>();
-        if (nodes != null) {
-            for (Node node: nodes) {
-                if (node.next.isEmpty()) {
-                    need.add(node.inputName);
-                }
-            }
-        }
-        return need;
-    }
-
     private ArrayList<StringBuilder> getAllStrings(Node tree) {
         ArrayList<StringBuilder> a = new ArrayList<StringBuilder>();
         ArrayList<StringBuilder> c = new ArrayList<StringBuilder>();
@@ -66,6 +42,7 @@ public class PrefixTrie {
             for (Node i: tree.next) {
                 a.addAll(getAllStrings(i));
             }
+            if (tree.leaf && !tree.next.isEmpty()) a.add(new StringBuilder(tree.inputName));
             for (StringBuilder i: a) {
                 StringBuilder d = new StringBuilder();
                 d.append(tree.inputName);
@@ -73,39 +50,6 @@ public class PrefixTrie {
             }
         }
         return c;
-    }
-
-    private static ArrayList<Integer> indNameOfEmptyEnd(List<Node> nodes) {
-        int a = 0;
-        ArrayList<Integer> need = new ArrayList<Integer>();
-        if (nodes != null) {
-            for (Node node: nodes) {
-                if (node.next.isEmpty()) {
-                    need.add(a);
-                }
-                a += 1;
-            }
-        }
-        return need;
-    }
-
-    private static int findIndOfEmpty(List<Node> nodes, char c){
-        ArrayList<Integer> list1 = indNameOfEmptyEnd(nodes);
-        ArrayList<Character> list2 = endIsEmpty(nodes);
-        int a = 0;
-        for (Character i: list2) {
-            if (i == c) return list1.get(a);
-            a += 1;
-        }
-        return -1;
-    }
-
-    private static int indOfLetter (List<Node> nodes, char c) {
-        int a = 0;
-        for (Node node: nodes) {
-            if (node.inputName == c && node.next.size() != 0) return a; else a += 1;
-        }
-        return -1;
     }
 
     private static Node getNode(List<Node> nodes, char c) {
@@ -119,126 +63,124 @@ public class PrefixTrie {
     }
 
     public boolean put (String s) {
-        try {
-
-            ThrowEx(s);
-            Node begin = root;
-            int a = 0;
-            for (char letter : s.toLowerCase().toCharArray()) {
-                if (!begin.next.contains(new Node(letter, new ArrayList<Node>()))) {
-                    begin.next.add(new Node(letter, new ArrayList<Node>()));
-                } else if (begin.next.contains(new Node(letter, new ArrayList<Node>())) && s.length() - 1 == a && !endIsEmpty(begin.next).contains(letter)) {
-                    begin.next.add(new Node(letter, new ArrayList<Node>()));
-                } else if (endIsEmpty(begin.next).contains(letter) && s.length() - 1 == a) {
-                    return false;
-                }
-                a += 1;
-                begin = getNode(begin.next, letter);
+        if (ThrowEx(s)) throw new IllegalArgumentException();
+        Node begin = root;
+        int a = 0;
+        for (char letter : s.toLowerCase().toCharArray()) {
+            if (!begin.next.contains(new Node(letter, new ArrayList<Node>(), false))) {
+                begin.next.add(new Node(letter, new ArrayList<Node>(), false));
+            } else if (begin.next.contains(new Node(letter, new ArrayList<Node>(), false)) && s.length() - 1 == a) {
+                if (getNode(begin.next, letter).leaf) return false;
             }
-            return true;
-        } catch (IllegalArgumentException e) {
-            throw  new IllegalArgumentException();
+            a += 1;
+            begin = getNode(begin.next, letter);
         }
+        begin.leaf = true;
+        return true;
     }
-
 
     public boolean delete (String s) {
-        try {
-            ThrowEx(s);
-            ArrayList<Character> lettersToDelete = new ArrayList<Character>();
-            Node startOfTree = root;
-            int ind = 0;
-            int save = 0;
-            int len = s.length();
-            for (char letter : s.toLowerCase().toCharArray()) { //находим номер уровня и букву, которую надо удалить
-                if (startOfTree.next.contains(new Node(letter, new ArrayList<Node>()))) {
-                    if (startOfTree.next.size() == 1 && (lettersToDelete.isEmpty())) {
-                        lettersToDelete.add(letter);
-                        ind = save;
-                    } else if (startOfTree.next.size() > 1) {
-                        lettersToDelete.clear();
-                        ind = save;
-                        lettersToDelete.add(letter);
+        if (ThrowEx(s)) throw new IllegalArgumentException();
+        ArrayList<Character> lettersToDelete = new ArrayList<Character>();
+        Node startOfTree = root;
+        int ind = 0;
+        int save = 0;
+        int len = s.length();
+        boolean empty = false; // обозначает, что надо удалять не полное слово
+        for (char letter : s.toLowerCase().toCharArray()) { //находим номер уровня и букву, которую надо удалить или убрать флаг "окончания"
+            if (startOfTree.next.contains(new Node(letter, new ArrayList<Node>(), false))) {
+                if (startOfTree.next.size() == 1) {
+                    if (lettersToDelete.isEmpty() || (getNode(startOfTree.next, letter).leaf && len - 1 == save)) {
+                        empty = false;
+                        if (getNode(startOfTree.next, letter).leaf && len - 1 == save && !getNode(startOfTree.next, letter).next.isEmpty()) {
+                            lettersToDelete.clear();
+                            empty = true;
+                            lettersToDelete.add(letter);
+                            ind = save;
+                        }
+                        if (lettersToDelete.isEmpty()) {
+                            lettersToDelete.add(letter);
+                            ind = save;
+                        }
                     }
-                } else return false;
-                save = save + 1;
-                startOfTree = getNode(startOfTree.next, letter);
-            }
-            Node begin = root;
-            int level = 0;
-            for (char letterOne : s.toLowerCase().toCharArray()) { //удаление не нужной ноды
-                if (begin.next.contains(new Node(letterOne, new ArrayList<Node>())) && !lettersToDelete.isEmpty() && letterOne == lettersToDelete.get(0) && level == ind) {
-                    if (endIsEmpty(begin.next).contains(letterOne) && len - 1 == level) { // случай, если у нас пустые дети
-                        begin.next.remove(findIndOfEmpty(begin.next, letterOne));
-                        break;
-                    } else {
-                        begin.next.remove(indOfLetter(begin.next, letterOne)); // случай обычный с детьми
-                    }
-                } else if (begin.next.contains(new Node(letterOne, new ArrayList<Node>()))) {
-                    begin = getNode(begin.next, letterOne);// переход дальше
+                } else if (startOfTree.next.size() > 1) {
+                    lettersToDelete.clear();
+                    ind = save;
+                    lettersToDelete.add(letter);
+                    if (getNode(startOfTree.next, letter).leaf && len - 1 == save) empty = true;
+                    else empty = false;
                 }
-                level += 1;
-            }
-            return true;
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException();
+            } else return false;
+            save = save + 1;
+            startOfTree = getNode(startOfTree.next, letter);
         }
+        Node begin = root;
+        int level = 0;
+        for (char letterOne : s.toLowerCase().toCharArray()) { //удаление не нужной ноды
+            if (begin.next.contains(new Node(letterOne, new ArrayList<Node>(), false)) && !lettersToDelete.isEmpty()
+                    && letterOne == lettersToDelete.get(0) && level == ind) {
+                if (!getNode(begin.next, letterOne).leaf && len - 1 == level) { // случай, если у нас пустые дети
+                    begin.next.remove(new Node(letterOne, new ArrayList<>(), false));
+                    break;
+                } else if (empty && len - 1  == level) {
+                    getNode(begin.next, letterOne).leaf = false;
+                    break;
+                } else {
+                    begin.next.remove(getNode(begin.next, letterOne));
+                    break;// случай обычный с детьми
+                }
+            } else if (begin.next.contains(new Node(letterOne, new ArrayList<Node>(), false))) {
+                begin = getNode(begin.next, letterOne);// переход дальше
+            }
+            level += 1;
+        }
+        return true;
     }
-
 
     public boolean find (String s) {
-        try {
-            ThrowEx(s);
-            int a = 0;
-            Node start = root;
-            int len = s.length();
-            for (char letter : s.toLowerCase().toCharArray()) {
-                if (!start.next.contains(new Node(letter, new ArrayList<Node>()))) {
+        if (ThrowEx(s)) throw new IllegalArgumentException();
+        int a = 0;
+        Node start = root;
+        int len = s.length();
+        for (char letter : s.toLowerCase().toCharArray()) {
+            if (!start.next.contains(new Node(letter, new ArrayList<Node>(), false))) {
+                return false;
+            } else {
+                if (getNode(start.next, letter).next.isEmpty() && a == len - 1) {
+                    return true;
+                } else if (a == len - 1 && !getNode(start.next, letter).next.isEmpty()) {
                     return false;
-                } else {
-                    if (endIsEmpty(start.next).contains(letter) && a == len - 1) {
-                        return true;
-                    } else if (a == len - 1 && !endIsEmpty(start.next).contains(letter)) {
-                        return false;
-                    } else start = getNode(start.next, letter);
-                }
-                a += 1;
+                } else if (a == len - 1 && getNode(start.next, letter).leaf) return true;
+                else start = getNode(start.next, letter);
             }
-            return true;
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException();
+            a += 1;
         }
+        return true;
     }
 
-
     public List<String> findAll (String s) {
-        try {
-            ArrayList<String> words = new ArrayList<String>();// слова, которые должны вывестись
-            Node start = root;
-            StringBuilder first = new StringBuilder();
+        ArrayList<String> words = new ArrayList<String>();// слова, которые должны вывестись
+        Node start = root;
+        StringBuilder first = new StringBuilder();
 
-            ThrowEx(s);
-            int len1 = s.length();
-            int count = 0;
-            for (char letter : s.toLowerCase().toCharArray()) { // ищем конечный узел от строки от заданной строки и прполняем слово
-                if (start.next.contains(new Node(letter, new ArrayList<Node>()))) {
-                    if (count != len1 - 1) first.append(letter);
-                    start = getNode(start.next, letter);
-                } else return words;
-                count += 1;
-            }
-
-            Node rem = start;
-            ArrayList<StringBuilder> futurePtTwo = getAllStrings(rem);
-            for (StringBuilder i : futurePtTwo) {
-                int len = i.length();
-                first.append(i);
-                words.add(first.toString());
-                first.delete(first.length() - len, first.length());
-            }
-            return words;
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException();
+        if (ThrowEx(s)) throw new IllegalArgumentException();
+        int len1 = s.length();
+        int count = 0;
+        for (char letter : s.toLowerCase().toCharArray()) { // ищем конечный узел от строки от заданной строки и прполняем слово
+            if (start.next.contains(new Node(letter, new ArrayList<Node>(), false))) {
+                if (count != len1 - 1) first.append(letter);
+                start = getNode(start.next, letter);
+            } else return words;
+            count += 1;
         }
+        Node rem = start;
+        ArrayList<StringBuilder> futurePtTwo = getAllStrings(rem);
+        for (StringBuilder i : futurePtTwo) {
+            int len = i.length();
+            first.append(i);
+            words.add(first.toString());
+            first.delete(first.length() - len, first.length());
+        }
+        return words;
     }
 }
--- src/test/java/PrefixTrie/PrefixTrieTest.java
+++ src/test/java/PrefixTrie/PrefixTrieTest.java
@@ -3,6 +3,7 @@ package PrefixTrie;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +16,10 @@ class PrefixTrieTest {
         assertTrue(tree.put("привет"));
         assertTrue(tree.put("таракашка"));
         assertTrue(tree.put("приз"));
+        assertTrue(tree.put("приве"));
+        assertTrue(tree.put("тр"));
+        assertFalse(tree.put("тр"));
+        assertFalse(tree.put("приве"));
         assertFalse(tree.put("привет"));
         assertThrows(IllegalArgumentException.class, () -> tree.put("."));
         assertThrows(IllegalArgumentException.class, () -> tree.put("8"));
@@ -26,9 +31,11 @@ class PrefixTrieTest {
         tree.put("привилегия");
         tree.put("трамвай");
         tree.put("тромб");
+        tree.put("тр");
+        assertTrue(tree.delete("тр"));
         assertTrue(tree.delete("трамвай"));
         assertTrue(tree.delete("тромб"));
-        assertFalse(tree.find("привет"));
+        assertTrue(tree.delete("привилегия"));
         assertThrows(IllegalArgumentException.class, () -> tree.delete("."));
         assertThrows(IllegalArgumentException.class, () -> tree.delete("356"));
         assertThrows(IllegalArgumentException.class, () -> tree.delete(""));
@@ -60,28 +67,36 @@ class PrefixTrieTest {
         tree.put("привередливые");
         tree.put("прирост");
         tree.put("привет");
-        tree.put("привгвоздить");
+        tree.put("пригвоздить");
         tree.put("привод");
         tree.put("приво");
-        ArrayList<String> expected1 = new ArrayList<String>();
-        expected1.add("привередливый");
-        expected1.add("привередливые");
-        expected1.add("прирост");
-        expected1.add("привет");
-        expected1.add("пригвоздить");
-        expected1.add("привод");
-        expected1.add("приво");
-        assertArrayEquals(expected1, (ArrayList<String>) tree.findAll("п"));
-        ArrayList<String> expected2 = new ArrayList<String>();
+        expected.clear();
+        expected.add("привередливый");
+        expected.add("привередливые");
+        expected.add("привет");
+        expected.add("привод");
+        expected.add("приво");
+        expected.add("прирост");
+        expected.add("пригвоздить");
+        assertArrayEquals(expected, (ArrayList<String>) tree.findAll("п"));
+        expected.clear();
+        assertArrayEquals(expected, (ArrayList<String>) tree.findAll("яр"));
         tree.put("метоксихлордиэтиламинометилбутинаминоакредин");
-        expected2.add("метоксихлордиэтиламинометилбутинаминоакредин");
-        assertArrayEquals(expected2, (ArrayList<String>) tree.findAll("м"));
+        expected.add("метоксихлордиэтиламинометилбутинаминоакредин");
+        assertArrayEquals(expected, (ArrayList<String>) tree.findAll("м"));
         assertThrows(IllegalArgumentException.class, () -> tree.findAll("."));
         assertThrows(IllegalArgumentException.class, () -> tree.findAll(""));
         assertThrows(IllegalArgumentException.class, () -> tree.findAll("6"));
     }
 
-    private boolean assertArrayEquals(ArrayList<String> expected, ArrayList<String> actual) {
-        return expected.equals(actual);
+    private boolean assertArrayEquals(List<String> expected, List<String> actual) {
+        if (expected.size() == actual.size()) {
+            for (int i = 0; i != expected.size(); i++) {
+                if (expected.get(i).equals(actual.get(i))) {
+                    continue;
+                } else throw new AssertionError();
+            }
+        } else throw new AssertionError();
+        return true;
     }
 }
