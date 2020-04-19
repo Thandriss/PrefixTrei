eafb52dd936deb3f43f0e634f850bda57a20e3a0
mahon <mahonko_yana@mail.ru>
2020-04-19 13:23:35 +0300

    доработки

--- src/main/java/PrefixTrie/PrefixTrie.java
+++ src/main/java/PrefixTrie/PrefixTrie.java
@@ -27,9 +27,11 @@ public class PrefixTrie {
     }
 
     Node root = new Node('.', new ArrayList<PrefixTrie.Node>(), false); //корень с разветвлениями
+
     private boolean ThrowEx (String s) {
         return s.length() == 0 || s.equals(".") || s.matches("[0-9]+");
     }
+
     private ArrayList<StringBuilder> getAllStrings(Node tree) {
         ArrayList<StringBuilder> a = new ArrayList<StringBuilder>();
         ArrayList<StringBuilder> c = new ArrayList<StringBuilder>();
@@ -42,7 +44,7 @@ public class PrefixTrie {
             for (Node i: tree.next) {
                 a.addAll(getAllStrings(i));
             }
-            if (tree.leaf && !tree.next.isEmpty()) a.add(new StringBuilder(tree.inputName));
+            if (tree.leaf && !tree.next.isEmpty()) a.add(new StringBuilder());
             for (StringBuilder i: a) {
                 StringBuilder d = new StringBuilder();
                 d.append(tree.inputName);
@@ -80,63 +82,61 @@ public class PrefixTrie {
     }
 
     public boolean delete (String s) {
-        if (ThrowEx(s)) throw new IllegalArgumentException();
-        ArrayList<Character> lettersToDelete = new ArrayList<Character>();
-        Node startOfTree = root;
-        int ind = 0;
-        int save = 0;
+        if (ThrowEx(s)) throw new IllegalArgumentException();//проверка на исключение
+        ArrayList<Character> lettersToDelete = new ArrayList<Character>();// сохранение буквы, которую надо удалить
+        Node startOfTree = root;//корень
+        int ind = 0;// сохранённый индекс уровня для удаления ноды
+        int save = 0;// текущий индекс уровня в первом цикле,
         int len = s.length();
-        boolean empty = false; // обозначает, что надо удалять не полное слово
+        boolean empty = false; //этот флаг обозначает, что надо удалять не полное слово, а всего лишь убрать флаг окончания
         for (char letter : s.toLowerCase().toCharArray()) { //находим номер уровня и букву, которую надо удалить или убрать флаг "окончания"
-            if (startOfTree.next.contains(new Node(letter, new ArrayList<Node>(), false))) {
-                if (startOfTree.next.size() == 1) {
+            if (startOfTree.next.contains(new Node(letter, new ArrayList<Node>(), false))) {//проверка на наличие буквы
+                if (startOfTree.next.size() == 1) {// условие, если в ассоциативном массиве один элемент
                     if (lettersToDelete.isEmpty() || (getNode(startOfTree.next, letter).leaf && len - 1 == save)) {
                         empty = false;
-                        if (getNode(startOfTree.next, letter).leaf && len - 1 == save && !getNode(startOfTree.next, letter).next.isEmpty()) {
-                            lettersToDelete.clear();
-                            empty = true;
-                            lettersToDelete.add(letter);
-                            ind = save;
+                        if (getNode(startOfTree.next, letter).leaf && len - 1 == save && !getNode(startOfTree.next, letter).next.isEmpty()) {//если ключевое слово заканчивается, а последняя буква слова в дереве имеет на пустые массивы дальше
+                            lettersToDelete.clear();//удаляем, сохранённые буквы раньше
+                            empty = true;//флаг, что надо убрать окончание слова(поменять с true на false)
+                            lettersToDelete.add(letter);//добавить букву, котовая будет "удалена", как окончание слова
+                            ind = save;//сохраняем индекс уровня, на котором находится последняя буква
                         }
-                        if (lettersToDelete.isEmpty()) {
-                            lettersToDelete.add(letter);
-                            ind = save;
+                        if (lettersToDelete.isEmpty()) {//какая-то первоначальная буква, которая, возможно будет удалена
+                            lettersToDelete.add(letter);//сохраняем букву
+                            ind = save;//сохраняем индекс
                         }
                     }
-                } else if (startOfTree.next.size() > 1) {
-                    lettersToDelete.clear();
-                    ind = save;
-                    lettersToDelete.add(letter);
-                    if (getNode(startOfTree.next, letter).leaf && len - 1 == save) empty = true;
-                    else empty = false;
+                } else if (startOfTree.next.size() > 1) {// условие, если в ассоциативном массиве больше одного элемента
+                    lettersToDelete.clear();//"забываем" буквы до
+                    ind = save;// сохраняем индекс
+                    lettersToDelete.add(letter);// сохраняем новую букву
+                    if (getNode(startOfTree.next, letter).leaf && len - 1 == save && !getNode(startOfTree.next, letter).next.isEmpty()) empty = true;//флаг, что надо убрать окончание слова(поменять с true на false)
+                    else empty = false;//флаг, что ничего убирать не надо
                 }
-            } else return false;
-            save = save + 1;
-            startOfTree = getNode(startOfTree.next, letter);
+            } else return false; // если нет буквы, то нет удаления
+            save = save + 1;//текущий индекс уровня
+            startOfTree = getNode(startOfTree.next, letter);//переход на новый узел по букве
         }
-        Node begin = root;
-        int level = 0;
+        Node begin = root;//заново проходим по всему дереву
+        int level = 0;//первоначальный уровень
         for (char letterOne : s.toLowerCase().toCharArray()) { //удаление не нужной ноды
             if (begin.next.contains(new Node(letterOne, new ArrayList<Node>(), false)) && !lettersToDelete.isEmpty()
-                    && letterOne == lettersToDelete.get(0) && level == ind) {
-                if (!getNode(begin.next, letterOne).leaf && len - 1 == level) { // случай, если у нас пустые дети
-                    begin.next.remove(new Node(letterOne, new ArrayList<>(), false));
+                    && letterOne == lettersToDelete.get(0) && level == ind) {// условие, при котором находится уровень и буква,которую надо удалить, чтобы удалилось слово
+                if (empty && len - 1  == level) {//случай, если нам надо убрать флаг об "окончании" слова
+                    getNode(begin.next, letterOne).leaf = false;//изменяем флаг
                     break;
-                } else if (empty && len - 1  == level) {
-                    getNode(begin.next, letterOne).leaf = false;
-                    break;
-                } else {
-                    begin.next.remove(getNode(begin.next, letterOne));
+                } else {//случаи, когда буква, которую надо удалить находится нанужном нам уровне, и нам всё равно, есть ли дети. Она должна быть удалена
+                    begin.next.remove(getNode(begin.next, letterOne));// удаление
                     break;// случай обычный с детьми
                 }
-            } else if (begin.next.contains(new Node(letterOne, new ArrayList<Node>(), false))) {
+            } else if (begin.next.contains(new Node(letterOne, new ArrayList<Node>(), false))) {//условие перехода на следующий узел
                 begin = getNode(begin.next, letterOne);// переход дальше
             }
-            level += 1;
+            level += 1;//изменение текущего уровня
         }
-        return true;
+        return true;//удаление проведено успешно
     }
 
+    
     public boolean find (String s) {
         if (ThrowEx(s)) throw new IllegalArgumentException();
         int a = 0;
@@ -147,7 +147,7 @@ public class PrefixTrie {
                 return false;
             } else {
                 if (getNode(start.next, letter).next.isEmpty() && a == len - 1) {
-                    return true; 
+                    return true;
                 } else if (a == len - 1 && !getNode(start.next, letter).next.isEmpty()) {
                     return false;
                 } else if (a == len - 1 && getNode(start.next, letter).leaf) return true;
